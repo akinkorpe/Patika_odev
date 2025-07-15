95b527e76d7d37e566dba62837efa3cbb86e6efd
Akin <akn.korpe6@gmail.com>
2025-07-15 19:24:54 +0300

    Add files via upload

---
+++ Patika_odev2.sql
@@ -0,0 +1,18 @@
+-- 1. konu 
+
+select * 
+from film
+where replacement_cost  
+between  12.99 and  16.99;
+-- 2. Konu
+
+SELECT first_name, last_name
+FROM actor
+WHERE first_name IN ('Penelope', 'Nick', 'Ed');
+
+-- 3. Konu
+
+SELECT *
+FROM film
+WHERE rental_rate IN (0.99, 2.99, 4.99)
+  AND replacement_cost IN (12.99, 15.99, 28.99);
