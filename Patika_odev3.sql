73f34b91de5445cdbcb8f00da565e2715dc6b3ca
Akin <akn.korpe6@gmail.com>
2025-07-19 18:54:40 +0300

    Add files via upload

---
+++ Patika_odev3.sql
@@ -0,0 +1,25 @@
+-- ### Ödev3 ### --
+
+-- 1. Soru: country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
+select country 
+from country
+where country like 'A%a'
+-------------------------
+-- 2. Soru: country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+select country 
+from country
+where country like '_____n';
+-------------------------
+--3. Soru: film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
+select title
+from film 
+where title ilike '%T%'
+limit 4;
+------------------------
+-- 4. Soru: film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+SELECT *
+FROM film
+WHERE title LIKE 'C%'
+  AND length > 90
+  AND rental_rate = 2.99;
+ -----------------------
